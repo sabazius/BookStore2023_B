b5197e332134c615d724f363904277a0ecd57a68
Boris Serafimov <b.serafimov@draftkings.com>
2023-12-14 18:50:47 +0200

    BookController

---
+++ BookStore/BookStore.DL/Interfaces/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Models.Models;
+using BookStore.Models.Models.Users;
+
+namespace BookStore.DL.Interfaces
+{
+    public interface IBookRepository
+    {
+        List<Book> GetAll();
+
+        Book GetById(int id);
+
+        void Add(Book book);
+
+        void Remove(int  id);
+    }
+}
---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.BL.Interfaces;
+using BookStore.Models.Models;
+using BookStore.Models.Models.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("GetAll")]
+        public List<Book> GetAll()
+        {
+            return _bookService.GetAll();
+        }
+
+        [HttpGet("GetById")]
+        public Book GetById(int id)
+        {
+            return _bookService.GetById(id);
+        }
+
+        [HttpPost("Add")]
+        public void Add([FromBody] Book book)
+        {
+            _bookService.Add(book);
+        }
+
+        [HttpDelete("Delete")]
+        public void Delete(int id)
+        {
+            _bookService.Remove(id);
+        }
+    }
+}
